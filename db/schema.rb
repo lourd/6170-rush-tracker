7b237eea3569ce58b1c255593e4c61d2088b351a
Dennis Smiley <djsmiley@mit.edu>
2013-11-12 18:34:51 -0500

    Added fields to Brother via a migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112232806) do
+ActiveRecord::Schema.define(version: 20131112233252) do
 
   create_table "brothers", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "fraternity_id"
+    t.boolean  "is_admin",               default: false
+    t.boolean  "is_verified",            default: false
   end
 
   add_index "brothers", ["email"], name: "index_brothers_on_email", unique: true
