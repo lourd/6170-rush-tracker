32905f6d30f2fc847941270dd7fe38bc3647809a
Dennis Smiley <djsmiley@mit.edu>
2013-11-16 15:36:23 -0500

    Administrator can create new Brother accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,7 @@
 class AccountsController < ApplicationController
   # require the current user to be authenticated to access this controller
   before_filter :authenticate_brother!
-
+  
   def index
     @brothers = Brother.findAllByFraternityID current_brother.fraternity_id
   
@@ -21,6 +21,28 @@ class AccountsController < ApplicationController
       return
     end
 
+  end
 
+  def processInvite
+    @firstname = params[:brother][:firstname]
+    @lastname = params[:brother][:lastname] 
+    @email = params[:brother][:email]
+
+    brother = Brother.new( 
+                  :firstname => @firstname, 
+                  :lastname  => @lastname,
+                  :email => @email,
+                  :password => params[:brother][:password],
+                  :password_confirmation => params[:brother][:password],
+                  :fraternity_id => current_brother.fraternity_id,
+                  :is_verified => true
+                    )
+    brother.save()
+  end
+
+  private
+
+  def post_params
+    params.require(:brother).permit(:firstname, :lastname, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Djsmiley0LourdBshaibuMattyhkFinal::Application.routes.draw do
   get "accounts/detail"
   get "accounts/verify"
   get "accounts/invite"
+  post "accounts/invite" => "accounts#processInvite"
+
   devise_for :brothers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
