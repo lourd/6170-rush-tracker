500b65d9ba0fb87cfc28a11564d3297b3c5bd430
mattyhk <mattkerr@mit.edu>
2013-11-16 16:16:27 -0500

    Can add primary contact by email but it is not robust

--- app/controllers/rushees_controller.rb
+++ app/controllers/rushees_controller.rb
@@ -15,11 +15,15 @@ class RusheesController < ApplicationController
 	end
 
 	def create
-	    @rushee = Rushee.new(rushee_params.merge(:fraternity_id => current_brother.fraternity_id, :action_status => "None", :bid_status => "None"))
-
+			primary_contact = Brother.find_by_email(rushee_params[:primary_contact_brother])
+			if primary_contact
+	    	@rushee = Rushee.new(rushee_params.merge(:fraternity_id => current_brother.fraternity_id, :primary_contact_id => primary_contact.id, :action_status => "None", :bid_status => "None"))
+	    else
+	    	@rushee = Rushee.new(rushee_params.merge(:fraternity_id => current_brother.fraternity_id, :action_status => "None", :bid_status => "None"))
+	    end
 	    respond_to do |format|
 	      if @rushee.save
-	        format.html { redirect_to @rushee, notice: 'Event was successfully created.' }
+	        format.html { redirect_to @rushee, notice: 'Rushee was successfully created.' }
 	        format.json { render action: 'show', status: :created, location: @rushee }
 	      else
 	        format.html { render action: 'new' }
@@ -41,7 +45,7 @@ class RusheesController < ApplicationController
 	private
 	# Never trust parameters from the scary internet, only allow the white list through.
 	  def rushee_params
-	    params.require(:rushee).permit(:firstname, :lastname, :email, :cellphone, :facebook_url, :twitter_url, :profile_picture_url, :dorm, :room_number, :hometown, :sports, :frats_rushing, :primary_contact_id)
+	    params.require(:rushee).permit(:firstname, :lastname, :email, :cellphone, :facebook_url, :twitter_url, :profile_picture_url, :dorm, :room_number, :hometown, :sports, :frats_rushing, :primary_contact_id, :primary_contact_brother)
 	  end
 
 	    # Use callbacks to share common setup or constraints between actions.
