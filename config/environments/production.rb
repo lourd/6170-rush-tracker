d2a667c26b743abc8a1b7955fc94fe16e0ada5d5
lourd <descioli@mit.edu>
2013-11-17 16:55:31 -0500

    More work on AWS working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,6 @@ Djsmiley0LourdBshaibuMattyhkFinal::Application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+  AWS.config(:access_key_id, :secret_access_key)
 }
 end
