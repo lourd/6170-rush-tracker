0726689a17982a6537eec39ecc95dbbab0da212e
lourd <descioli@mit.edu>
2013-11-17 16:47:24 -0500

    More work on s3 configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,14 @@ Djsmiley0LourdBshaibuMattyhkFinal::Application.configure do
 
   # Paperclip settings
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
